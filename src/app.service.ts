2398c632f3517584e0b2b98b49839198576f2da4
ralf-boltshauser <ra.boltshauser3@gmail.com>
2021-07-06 09:41:09 +0200

    Merge

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,10 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-<<<<<<< HEAD
     return 'Very Nice!';
-=======
-    return 'Hello World!';
->>>>>>> e35d04e (Initial Commit)
   }
 }
