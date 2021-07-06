1cfec28110461219a1e4cf37687d5c275ad97313
ralf-boltshauser <ra.boltshauser3@gmail.com>
2021-07-06 09:32:48 +0200

    Initial Commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+<<<<<<< HEAD
     return 'Very Nice!';
+=======
+    return 'Hello World!';
+>>>>>>> e35d04e (Initial Commit)
   }
 }
